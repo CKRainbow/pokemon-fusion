c1914ebc0970fc6c3f9132882669b3a64f32c668
CKRainbow <nanashichi@proton.me>
2024-01-21 10:58:47 +0800

    fix: 真正修复无法锁定身体随机头部的问题 fix: 修复所有简中译名问题 fix: 适应了全国图鉴编号对应多个pif编号的问题 format: 格式化，并添加.prettierrc文件

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,19 @@
-import { Context, Schema, h } from 'koishi'
-import { getPifUrl, getPifUrlAll, getPokeNameByPifId, randFuse, randFuseByBody, randFuseByHead, tryParseIntoPifId } from './utils'
+import { Context, Schema, h } from "koishi";
+import { getPifUrl, getPifUrlAll, getPokeNameByPifId, randFuse, randFuseByBody, randFuseByHead, tryParseIntoPifId } from "./utils";
 
-export const name = 'pokemon-fusion'
+export const name = "pokemon-fusion";
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  // write your plugin here
-  ctx.command(
-    "fuse [head] [body]",
-    "获得某两个宝可梦的融合"
-  ).option(
-    "all",
-    "-a"
-  ).action(
-    async (argv, head, body) => {
+  ctx
+    .command("fuse [head] [body]", "获得某两个宝可梦的融合")
+    .option("all", "-a")
+    .action(async (argv, head, body) => {
       const options = argv.options;
-      
+
       let headId = tryParseIntoPifId(head);
       let bodyId = tryParseIntoPifId(body);
 
@@ -28,52 +23,47 @@ export function apply(ctx: Context) {
         return `尚不支持该身体宝可梦`;
       }
 
-      if (headId === undefined || (headId === 0 && bodyId === 0)) {
+      if (headId === undefined && bodyId === undefined) {
         [headId, bodyId] = randFuse();
-      } else if (bodyId == undefined || bodyId === 0) {
+      } else if (bodyId == undefined) {
         [headId, bodyId] = randFuseByHead(headId);
-      } else if (headId === 0) {
+      } else if (headId === undefined) {
         [headId, bodyId] = randFuseByBody(bodyId);
       }
 
-      argv.session.send(
-        `要融合的宝可梦原来是${getPokeNameByPifId(headId)}和${getPokeNameByPifId(bodyId)}！`
-      )
-      
+      argv.session.send(`要融合的宝可梦原来是${getPokeNameByPifId(headId)}和${getPokeNameByPifId(bodyId)}！`);
+
       let status = 0;
       let url = undefined;
       if (options.all) {
         url = getPifUrlAll(headId, bodyId);
         status = 200;
-      }
-      else {
+      } else {
+        // TODO: 支持fetch后替换或采用内部表避免不存在融合
+        // 使用内部表还可以方便地支持变体
         url = getPifUrl(headId, bodyId);
-        status = await ctx.http.get(url).then(
-          (res) => {
+        status = await ctx.http
+          .get(url)
+          .then((res) => {
             return 200;
-          }
-        ).catch(
-          (e) => {
+          })
+          .catch((e) => {
             return e.response.status;
-          }
-        )
+          });
       }
 
       if (status === 200) {
-        return h('img', { src: url });
+        return h("img", { src: url });
       } else if (status === 404) {
-        return `暂时没有这种融合呢。`
+        return `暂时没有这种融合呢。`;
       } else {
-        return `出错了！`
+        return `出错了！`;
       }
-    }
-  ).alias(
-    "随机融合", {args: ["0", "0"]}
-  ).alias(
-    "全随机融合", {args: ["0", "0"], options: ["--all"]}
-  ).alias(
-    "融合"
-  ).usage(
-    "第一个参数代表头部，第二个参数代表身体，可以使用图鉴编号、中文名或英文名。\n输入0代表随机，参数--all代表无视人工图过滤。"
-  )
+    })
+    .alias("随机融合", { args: ["0", "0"] })
+    .alias("全随机融合", { args: ["0", "0"], options: { all: true } })
+    .shortcut(/^锁头 (\b.*\b).*$/, { args: ["$1", "0"] })
+    .shortcut(/^锁身 (\b.*\b).*$/, { args: ["0", "$1"] })
+    .alias("融合")
+    .usage("第一个参数代表头部，第二个参数代表身体，可以使用图鉴编号、中文名或英文名。\n输入0代表随机，参数--all代表无视人工图过滤。");
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,110 +1,87 @@
 import { Random, isInteger } from "koishi";
-import {ZhName, EnName, pifIdMax, pokeIdToPifIdMap, pifIdToPokeIdMap} from './consts';
+import { ZhName, EnName, pifIdMax, pokeIdToPifIdMap, pifIdToPokeIdMap, PifId, PokeId } from "./consts";
 
-type PokeId = number;
-type PifId = number;
 
 // const validFuseMap: Map<PifId, Array<PifId>> = {
 
 // }
 
+export function tryParseIntoPifId(parsee: string | undefined): PifId | undefined | null {
+  if (parsee === undefined) return undefined;
 
-export function tryParseIntoPifId(parsee: string | undefined): PifId | undefined | null
-{
-    if (parsee === undefined)
-        return undefined;
+  let pokeId = 0;
 
-    let pokeId = 0;
+  if (isInteger(parseFloat(parsee))) {
+    pokeId = parseInt(parsee);
+    if (pokeId === 0) return undefined;
+  } else if (typeof parsee === "string") {
+    pokeId = tryGetPokeIdFromName(parsee);
+    if (pokeId === null) return null;
+  } else {
+    return null;
+  }
 
-    if (isInteger(parseFloat(parsee))) {
-        pokeId = parseInt(parsee);
-    } else if (typeof parsee === "string") {
-        pokeId = getPokeIdFromName(parsee);
-        if (pokeId === null)
-            return null;
+  return tryGetPifIdFromPokeId(pokeId);
+}
+
+export function randFuse(): Array<PifId> {
+  const randHead = Random.int(1, pifIdMax);
+  const randBody = Random.int(1, pifIdMax);
+  return [randHead, randBody];
+}
+
+export function randFuseByHead(head: PifId): Array<PifId> {
+  const randBody = Random.int(1, pifIdMax);
+  return [head, randBody];
+}
+
+export function randFuseByBody(body: PifId): Array<PifId> {
+  const randHead = Random.int(1, pifIdMax);
+  return [randHead, body];
+}
+
+function tryGetPokeIdFromName(name: string): PokeId | null {
+  name = name.toLocaleLowerCase();
+  if (ZhName.indexOf(name) >= 0) return ZhName.indexOf(name) + 1;
+  else if (EnName.indexOf(name) >= 0) return EnName.indexOf(name) + 1;
+  else return null;
+}
+
+function tryGetPifIdFromPokeId(pokeId: PokeId): PifId | null {
+  if (pokeId <= -1) return null;
+  else if (pokeId <= 251) return pokeId; // 初代和二代宝可梦Id相同
+  else if (pokeIdToPifIdMap[pokeId]) {
+    const pifIds = pokeIdToPifIdMap[pokeId];
+    if (pifIds instanceof Array) {
+      return pifIds[Random.int(pifIds.length)];
     } else {
-        return null
+      return pifIds;
     }
-
-    return tryGetPifIdFromPokeId(pokeId);
+  } else return null;
 }
 
-export function randFuse(): Array<PifId>
-{
-    const randHead = Random.int(1, pifIdMax);
-    const randBody = Random.int(1, pifIdMax);
-    return [randHead, randBody];
+function tryGetPokeIdFromPifId(pifId: PifId): PokeId | null {
+  if (pifId === 0 || pifId < -1) return null;
+  else if (pifId <= 251) return pifId; // 初代和二代宝可梦Id相同
+  else if (pifIdToPokeIdMap[pifId]) return pifIdToPokeIdMap[pifId];
+  else return null;
 }
 
-export function randFuseByHead(head: PifId): Array<PifId>
-{
-    const randBody = Random.int(1, pifIdMax);
-    return [head, randBody];
+export function getPokeNameByPifId(pifId: PifId): string {
+  const pokeId = tryGetPokeIdFromPifId(pifId);
+  if (pokeId === null) return "Error";
+
+  return getPokeNameByPokeId(pokeId);
 }
 
-export function randFuseByBody(body: PifId): Array<PifId>
-{
-    const randHead = Random.int(1, pifIdMax);
-    return [randHead, body];
+export function getPokeNameByPokeId(pokeId: PokeId): string {
+  return ZhName[pokeId - 1];
 }
 
-function getPokeIdFromName(name: string): PokeId | null
-{
-    name = name.toLocaleLowerCase();
-    if (ZhName.indexOf(name) >= 0)
-        return ZhName.indexOf(name) + 1;
-    else if (EnName.indexOf(name) >= 0)
-        return EnName.indexOf(name) + 1;
-    else
-        return null;
+export function getPifUrlAll(head: PifId, body: PifId): string {
+  return `https://gitlab.com/pokemoninfinitefusion/autogen-fusion-sprites/-/raw/master/Battlers/${head}/${head}.${body}.png?ref_type=heads`;
 }
 
-function tryGetPifIdFromPokeId(pokeId: PokeId): PifId | null
-{
-    if (pokeId <= -1)
-        return null;
-    else if (pokeId <= 251)
-        // 初代和二代宝可梦Id相同
-        return pokeId;
-    else if (pokeIdToPifIdMap[pokeId])
-        return pokeIdToPifIdMap[pokeId]
-    else
-        return null;
-}
-
-function tryGetPokeIdFromPifId(pifId: PifId): PokeId | null
-{
-    if (pifId === 0 || pifId < -1)
-        return null;
-    else if (pifId <= 251)
-        // 初代和二代宝可梦Id相同
-        return pifId;
-    else if (pifIdToPokeIdMap[pifId])
-        return pifIdToPokeIdMap[pifId]
-    else
-        return null;
-}
-
-export function getPokeNameByPifId(pifId: PifId): string
-{
-    const pokeId = tryGetPokeIdFromPifId(pifId);
-    if (pokeId === null)
-        return "Error";
-    
-    return getPokeNameByPokeId(pokeId);
-}
-
-export function getPokeNameByPokeId(pokeId: PokeId): string
-{
-    return ZhName[pokeId - 1];
-}
-
-export function getPifUrlAll(head: PifId, body: PifId): string
-{
-    return `https://gitlab.com/pokemoninfinitefusion/autogen-fusion-sprites/-/raw/master/Battlers/${head}/${head}.${body}.png?ref_type=heads`
-}
-
-export function getPifUrl(head: PifId, body: PifId): string
-{
-    return `http://gitlab.com/pokemoninfinitefusion/customsprites/-/raw/master/CustomBattlers/${head}.${body}.png?ref_type=heads`;
+export function getPifUrl(head: PifId, body: PifId): string {
+  return `http://gitlab.com/pokemoninfinitefusion/customsprites/-/raw/master/CustomBattlers/${head}.${body}.png?ref_type=heads`;
 }
